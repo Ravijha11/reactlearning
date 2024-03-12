e8fa91a84e96852a0643c3b05d3d7da2d54b1892
RAVINDRA JHA <ravijha110903@gmail.com>
2024-03-13 01:06:11 +0530

    todo app

---
+++ projects/todo-app-version1/src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <center class="todo-content-">
+      <h1>Todo app </h1>
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-4">col-sm</div>
+          <div class="col-4">col-sm</div>
+          <div class="col-2">col-sm</div>
+        </div>
+      </div>
+    </center>
+  );
+}
+
+export default App;
